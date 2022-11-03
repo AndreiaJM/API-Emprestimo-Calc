522060e688ec6fefab78eb3d36b3aece08849ea6
AndreiaJM <andreia.ajms@gmail.com>
2022-11-03 12:08:08 -0300

    First commit

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from flask_cors import CORS
+import os
+
+app = Flask(__name__)
+
+cors = CORS(app, resources={r"/*":{"origins":"*"}})
+
+@app.route('/', methods=['GET'])
+def index():
+    return 'ola mundo'
+
+
+def main():
+    port = int(os.environ.get("PORT",5000))
+    app.run(host='0.0.0.0', port=port)
+
+if __name__ == "__main__":
+    main()
